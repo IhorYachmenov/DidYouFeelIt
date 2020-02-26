e473d164c5e72ce1f0e63dddd0e875e2847be78a
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-02-26 16:18:36 +0200

    had add handling empty or null cases

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -70,17 +70,39 @@ public class MainActivity extends AppCompatActivity {
 
     private class EarthquakeAsyncTask  extends AsyncTask<String, Void, Event> {
 
+        /**
+         * This method is invoked (or called) on a background thread, so we can perform
+         * long-running operations like making a network request.
+         *
+         * It is NOT okay to update the UI from a background thread, so we just return an
+         * {@link Event} object as the result.
+         */
         @Override
         protected Event doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
 
             Event result = Utils.fetchEarthquakeData(urls[0]);
             return result;
         }
 
+        /**
+         * This method is invoked on the main UI thread after the background work has been
+         * completed.
+         *
+         * It IS okay to modify the UI within this method. We take the {@link Event} object
+         * (which was returned from the doInBackground() method) and update the views on the screen.
+         */
         @Override
-        protected void onPostExecute(Event event) {
+        protected void onPostExecute(Event result) {
             //super.onPostExecute(event);
-            updateUi(event);
+            // If there is no result, do nothing.
+            if (result == null) {
+                return;
+            }
+            updateUi(result);
         }
     }
 }
